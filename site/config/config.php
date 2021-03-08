e9f083b7580572b9f20896e53694ee155123cc69
Tim Sterker <tim.sterker@gleif.org>
2021-03-08 20:33:18 +0100

    Add beebmx/kirby-db and beebmx/kirby-env (remove kirby3-dotenv)

--- site/config/config.php
+++ site/config/config.php
@@ -1,5 +1,19 @@
 <?php
 
+\Beebmx\KirbyEnv::load(__DIR__ . '/../..');
+
+print_r([
+    'driver' => 'mysql',
+    'host' => env('DB_HOST'),
+    'port' => env('DB_PORT', '3306'),
+    'database' => env('DB_DATABASE'),
+    'username' => env('DB_USER'),
+    'password' => env('DB_PASSWORD'),
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'strict' => true,
+]);
+
 /**
  * The config file is optional. It accepts a return array with config options
  * Note: Never include more than one return statement, all options go within this single return array
@@ -9,4 +23,24 @@
  */
 return [
     'debug' => true,
+
+    'beebmx.kirby-db.default' => 'mysql',
+    'beebmx.kirby-db.drivers' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USER'),
+            'password' => env('DB_PASSWORD'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'strict' => true,
+        ],
+
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'database' => 'database.sqlite',  // Ensure file exists
+        ],
+    ],
 ];
